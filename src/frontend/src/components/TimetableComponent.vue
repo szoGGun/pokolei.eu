1f316fe740e09fe10a105ac51bf13c1bf54e1229
Przemysław Rutkowski <przrut4@gmail.com>
2023-01-22 04:41:04 +0100

    StationTimetable screen with TimetableComponent.vue added

---
+++ src/frontend/src/components/TimetableComponent.vue
@@ -0,0 +1,165 @@
+<template>
+  <div>
+    <div id="hud" class="d-flex align-items-center">
+      <div id="clock" class="text-sm-2">{{ currentTime }}</div>
+      <div id="stationInput" class="col-sm-4">
+        <input type="text" v-model="stationName" class="form-control" placeholder="Wpisz nazwę stacji...">
+      </div>
+      <div id="search" class="col-sm-6">
+        <button id="show-busses" class="btn btn-success mt-2 p-jc-center" @click="search()">Szukaj</button>
+      </div>
+    </div>
+    <div style="overflow-x:auto;">
+      <table class="hud-table">
+        <thead>
+        <tr>
+          <th>Czas</th>
+          <th>Pociąg</th>
+          <th>Kierunek</th>
+          <th>Peron/Platforma</th>
+          <th>Przewoźnik</th>
+          <th>Opóźnienie</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="departure in departures" :key="departure.id">
+          <td>{{ formatDate(departure.when) }}</td>
+          <td>{{ departure.line.name }}</td>
+          <td>{{ departure.direction }}</td>
+          <td>{{ departure.platform }}</td>
+          <td>{{ departure.line.productName }}</td>
+          <td>{{ formatTimeDifference(departure.plannedWhen, departure.when) }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      departures: [],
+      stationName: "",
+      currentTime: "",
+    }
+  },
+  created() {
+    setInterval(() => {
+      this.currentTime = new Date().toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      });
+    }, 1);
+  },
+  methods: {
+    async search() {
+      const res = await fetch("http://localhost:3080/departures/?station=" + this.stationName + "&amount=30");
+      this.departures = await res.json();
+    },
+    filterRemarks(departure) {
+      return departure.remarks.filter(remark => remark.code === "ZN");
+    },
+    formatDate(date) {
+      const formattedDate = new Date(date);
+      return formattedDate.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+    },
+    formatTimeDifference(planned, actual) {
+      const difference = (new Date(actual) - new Date(planned)) / 60000;
+      return difference ? (difference > 0 ? "+" : "-") + Math.abs(difference) : "";
+    },
+  }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap');
+
+#hud {
+  top: 0px;
+  background-color: #0000ff;
+  left: 0px;
+  width: 100%;
+  overflow: hidden;
+  color: white;
+  font-family: 'JetBrains Mono', monospace;
+  border-bottom: 1px solid black;
+}
+
+#clock {
+  font-size: 3rem;
+  padding-left: 20px;
+}
+
+#stationInput {
+  font-family: "Roboto", sans-serif;
+  padding-left: 50px;
+}
+
+#stationInput > input {
+  height: 100%;
+  font-size: 2rem;
+  background-color: #0000bb;
+  border: 0px;
+  color: white;
+  border-radius: 4px;
+  padding: 1px .2em;
+  width: calc(100% - 3.2rem);
+}
+
+.hud-table {
+  position: sticky;
+  top: 4.4rem;
+  width: 100%;
+  background-color: #0000bb;
+  color: white;
+  font-size: 1em;
+  font-family: "Roboto", sans-serif;
+  border-collapse: collapse;
+}
+
+tr:nth-child(even) {
+  background-color: #0000bb;
+}
+
+tr:nth-child(odd) {
+  background-color: #0000ff;
+}
+
+tr {
+  border-bottom: 1px solid black;
+  height: 5em;
+}
+
+th {
+  background-color: #0000bb;
+  font-size: 1.5em;
+}
+
+td {
+  font-size: 1.2em;
+}
+
+
+@media only screen and (max-width: 600px) {
+  #clock {
+    font-size: 1.5rem;
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  #stationInput {
+    font-size: 1.5rem;
+  }
+}
+
+
+@media only screen and (max-width: 600px) {
+  #search {
+    width: 50%;
+  }
+}
+</style>
---
+++ src/frontend/src/views/StationTimetable.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container-fluid m-0 p-0">
+    <div id="info" class="full-height p-5">
+      <div class="row pb-5 h-100 d-flex align-items-center justify-content-center">
+        <div class="col-lg-6 my-auto pt-4">
+            <div class="row p-5 mt-5">
+            <div class="container pt-5 border p-md-5 mt-5 bg-white rounded shadow">
+              <h1 class="ml-md-4 mr-md-4 p-3">Tablice Informacyjne</h1>
+              <h5 class="ml-md-4 mr-md-4 p-4">
+                Szukasz łatwego sposobu na sprawdzenie jakie pociągi odjeżdżają z twojej stacji?
+                Po prostu wpisz w pole poniżej nazwę stacji, która Cię interesuje, a my wyświetlimy wszystko czego Ci
+                potrzeba.
+                Spróbuj, to banalnie proste.
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <TimetableComponent/>
+
+
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Footer from "@/components/Footer.vue"
+import TimetableComponent from "@/components/TimetableComponent.vue";
+
+export default {
+  name: "StationTimetable",
+  components: {
+    Footer,
+    TimetableComponent,
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+#info {
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.9)), url('../assets/timetable.jpg') no-repeat center center;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+</style>
