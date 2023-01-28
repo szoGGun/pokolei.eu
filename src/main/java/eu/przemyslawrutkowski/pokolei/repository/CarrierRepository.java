59a4039fa0e898c9e22ed17e4c63e2a55076376e
Przemysław Rutkowski <przrut4@gmail.com>
2023-01-28 18:26:55 +0100

    Unused entity removed

--- src/main/java/eu/przemyslawrutkowski/pokolei/repository/CarrierRepository.java
+++
@@ -1,7 +0,0 @@
-package eu.przemyslawrutkowski.pokolei.repository;
-
-import eu.przemyslawrutkowski.pokolei.entity.Carrier;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarrierRepository extends JpaRepository<Carrier, Long> {
-}
