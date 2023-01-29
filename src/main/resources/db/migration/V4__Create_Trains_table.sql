b0ff45381e29bbb45c8ec4139a203a1a5e869d16
Przemysław Rutkowski <przrut4@gmail.com>
2023-01-29 19:21:04 +0100

    Changes in create Trains table migration file. More real data.

--- src/main/resources/db/migration/V4__Create_Trains_table.sql
+++ src/main/resources/db/migration/V4__Create_Trains_table.sql
@@ -3,11 +3,11 @@ CREATE TABLE trains
     train_id        SERIAL PRIMARY KEY,
     train_number    VARCHAR(30)  NOT NULL,
     train_name      VARCHAR(30),
-    route           VARCHAR(200) NOT NULL,
+    route           VARCHAR(500) NOT NULL,
     running_dates   VARCHAR(30)  NOT NULL,
     additional_info VARCHAR(200)
 );
 INSERT INTO trains (train_number, train_name, route, running_dates, additional_info)
-VALUES ('TRAIN1', 'Train Name 1', 'Route 1', 'Monday, Tuesday, Wednesday', 'Additional information for train 1'),
-       ('TRAIN2', 'Train Name 2', 'Route 2', 'Everyday', 'Additional information for train 2'),
-       ('TRAIN3', 'Train Name 2', 'Route 2', 'Monday', 'Additional information for train 3');
+VALUES ('(100) 44000', 'IC Moravia', 'Wien Hbf 18:10 - Břeclav - Přerov - Ostrava hl.n. - Bohumin (21:15/21:27) - Chałupki - Racibórz - Gliwice - Katowice 23:02', 'Monday, Tuesday, Wednesday', 'Zmiana kierunku lub lokomotywy na stacjach: Wien Hbf'),
+       ('(107) 54000/1', 'IC Sobieski', 'Gdynia Gł. 10:54 - Gdańsk Gł. - Tczew - Działdowo - Warszawa Centr. - Zawiercie - Sosonowiec Gł. - Katowice - Tychy - Rybnik - Chałupki - Bohumin (18:30/18:42) - Ostrava hl.n. - Přerov - Břeclav - Wien Hbf 21:49', 'Everyday', 'Zmiana kierunku lub lokomotywy na stacjach: Bohumin'),
+       ('(108) 14008/9', 'IC Silesia', 'Warszawa Wsch. 17:34 - Warszawa Centr. - Włoszczowa Płn. - Katowice - Tychy - Wodzisław Śl. - Chałupki - Bohumin 21:51', 'Monday', '');
