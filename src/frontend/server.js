397672c9ddeb47511739decfe4e0754392bbd631
Przemysław Rutkowski <prutkowski@stratoflow.com>
2023-02-19 22:37:07 +0100

    Refactor to remove duplicated code

--- src/frontend/server.js
+++ src/frontend/server.js
@@ -9,108 +9,72 @@ const client = createClient(pkpProfile, 'train-api');
 
 app.use(require('cookie-parser')());
 
-async function getNArrivals(station, n) {
-    let arrivals = [];
+async function getNStationInfo(station, n, endpoint) {
+    let stationInfo = [];
     for (let i = 0; i < n; i += 10) {
         try {
-            var batch = await client.arrivals(station, { products: {bus: 1, tram: 1}, results: 10, duration: 600, when: (arrivals.length === 0 ? new Date() : arrivals[arrivals.length - 1].when)});
+            var batch = await endpoint(station, { products: {bus: 1, tram: 1}, results: 10, duration: 600, when: (stationInfo.length === 0 ? new Date() : stationInfo[stationInfo.length - 1].when)});
         } catch (e) {
             var batch = [{direction: "Station not found", platform: "N/A", line: {mode: "special"}, when: new Date(2069, 1, 1)}];
         }
-        for (let arrival of batch) {
-            arrivals.push(arrival);
+        for (let info of batch) {
+            stationInfo.push(info);
         }
     }
-    const uniqueArrivals = [];
-    for (let arrival of arrivals) {
-        if (!uniqueArrivals.some(d => new Date(d.when).getTime() === new Date(arrival.when).getTime())) {
-            uniqueArrivals.push(arrival);
+    const uniqueStationInfo = [];
+    for (let info of stationInfo) {
+        if (!uniqueStationInfo.some(d => new Date(d.when).getTime() === new Date(info.when).getTime())) {
+            uniqueStationInfo.push(info);
         }
     }
-    return uniqueArrivals;
+    return uniqueStationInfo;
+}
+
+async function getNArrivals(station, n) {
+    return await getNStationInfo(station, n, client.arrivals);
 }
 
 async function getNDepartures(station, n) {
-    let departures = [];
-    for (let i = 0; i < n; i += 10) {
-        try {
-            var batch = await client.departures(station, { products: {bus: 1, tram: 1}, results: 10, duration: 600, when: (departures.length === 0 ? new Date() : departures[departures.length - 1].when)});
-        } catch (e) {
-            var batch = [{direction: "Station not found", platform: "N/A", line: {mode: "special"}, when: new Date(2069, 1, 1)}];
-        }
-        for (let departure of batch) {
-            departures.push(departure);
-        }
+    return await getNStationInfo(station, n, client.departures);
+}
+
+async function handleStationRequest(req, res, endpointFunc) {
+    try {
+        var { station, amount } = req.query;
+    } catch (error) {
+        res.send(error);
+        return false;
     }
-    const uniqueDepartures = [];
-    for (let departure of departures) {
-        if (!uniqueDepartures.some(d => new Date(d.when).getTime() === new Date(departure.when).getTime())) {
-            uniqueDepartures.push(departure);
-        }
+
+    // check if station and amount are set
+    if (!station || !amount) {
+        res.status(400).send([{direction: "Please enter a station name!", id: "0", platform: "400", line: {mode: "special"}, when: new Date(2069, 1, 1)}]);
+        return false;
     }
-    return uniqueDepartures;
+
+    // Check if a cookie with the station name already exists
+    if (req.cookies && req.cookies.stationName) {
+        res.status(429).send([{direction: "Too many requests!", id: "0", platform: "429", line: {mode: "special"}, when: new Date(2069, 1, 1)}]);
+        return;
+    } else {
+        res.cookie("stationName", station, { maxAge: 10000, sameSite: true });
+    }
+
+    console.log(String("New request for: " + station).padEnd(30));
+    client.locations(station).then((data) => {
+        var stationID = data[0].id;
+        endpointFunc(stationID, amount).then((results) => {
+            res.send(results);
+        });
+    });
 }
 
 app.get("/departures", async function (req, res) {
-    try {
-        var { station, amount } = req.query;
-    } catch (error) {
-        res.send(error);
-        return false;
-    }
-
-    // check if station and amount are set
-    if (!station || !amount) {
-        res.status(400).send([{direction: "Please enter a station name!", id: "0", platform: "400", line: {mode: "special"}, when: new Date(2069, 1, 1)}]);
-        return false;
-    }
-
-    // Check if a cookie with the station name already exists
-    if (req.cookies && req.cookies.stationName) {
-        res.status(429).send([{direction: "Too many requests!", id: "0", platform: "429", line: {mode: "special"}, when: new Date(2069, 1, 1)}]);
-        return;
-    } else {
-        res.cookie("stationName", station, { maxAge: 10000, sameSite: true });
-    }
-
-    console.log(String("New request for: " + station).padEnd(30));
-    client.locations(station).then((data) => {
-        var stationID = data[0].id;
-        getNDepartures(stationID, amount).then((departures) => {
-            res.send(departures);
-        });
-    });
+    handleStationRequest(req, res, getNDepartures);
 });
 
 app.get("/arrivals", async function (req, res) {
-    try {
-        var { station, amount } = req.query;
-    } catch (error) {
-        res.send(error);
-        return false;
-    }
-
-    // check if station and amount are set
-    if (!station || !amount) {
-        res.status(400).send([{direction: "Please enter a station name!", id: "0", platform: "400", line: {mode: "special"}, when: new Date(2069, 1, 1)}]);
-        return false;
-    }
-
-    // Check if a cookie with the station name already exists
-    if (req.cookies && req.cookies.stationName) {
-        res.status(429).send([{direction: "Too many requests!", id: "0", platform: "429", line: {mode: "special"}, when: new Date(2069, 1, 1)}]);
-        return;
-    } else {
-        res.cookie("stationName", station, { maxAge: 10000, sameSite: true });
-    }
-
-    console.log(String("New request for: " + station).padEnd(30));
-    client.locations(station).then((data) => {
-        var stationID = data[0].id;
-        getNArrivals(stationID, amount).then((arrivals) => {
-            res.send(arrivals);
-        });
-    });
+    handleStationRequest(req, res, getNArrivals);
 });
 
 app.listen(process.env.PORT || 3080, () => {
